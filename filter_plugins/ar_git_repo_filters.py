bbdb8c7e6fb838137ac8e6c17a62a884deeb809a
David Stewart <david.stewart@estafet.com>
2020-03-06 13:50:38 +0000

    provide alternate method to forcing ssh key using ssh/config file

---
+++ filter_plugins/ar_git_repo_filters.py
@@ -0,0 +1,37 @@
+import re
+
+def get_hostname(url):
+    if url.startswith('https'):
+      index=3
+    elif url.startswith('git'):
+      index = 1
+    elif url.startswith('ssh'):
+      index=4
+
+    return re.split('[@:/]', url)[index]
+
+class FilterModule(object):
+    '''
+    custom jinja2 filters for working with collections
+    '''
+
+    def filters(self):
+        return {
+            'get_hostname': get_hostname
+        }
+
+'''
+Testing
+'''
+import unittest
+
+
+class TestSelectFromArrayOfDicts(unittest.TestCase):
+    git_url = "git@git-host.domain:path/gitrepo.git"
+    https_url = "https://git-host.domain/path/gitrepo.git"
+    ssh_url = "ssh://git@git-host.domain/path/gitrepo.git"
+
+    def test_get_hostname_git(self):
+        self.assertEqual('git-host.domain', get_hostname(self.git_url))
+        self.assertEqual('git-host.domain', get_hostname(self.https_url))
+        self.assertEqual('git-host.domain', get_hostname(self.ssh_url))
